a723cd0dc8f37206615bb97cc52c56cecffe5c41
Alexander Konotop <alexander.konotop@gmail.com>
2018-08-19 15:03:15 +0300

    Cleaned a Users table migration from unused default implementation

--- database/migrations/20180819111611_create_users_table.php
+++ database/migrations/20180819111611_create_users_table.php
@@ -5,35 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    addCustomColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Any other destructive changes will result in an error when trying to
-     * rollback the migration.
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    /*public function change()
-    {
-
-    }*/
 
     public function up()
     {
@@ -48,8 +19,10 @@ class CreateUsersTable extends Migration
             $table->unique('email');
         });
     }
+
     public function down()
     {
         $this->schema->drop('users');
     }
+
 }
