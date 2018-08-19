f82abf4339cf7db999b674946dd29d5eb45e128f
Alexander Konotop <alexander.konotop@gmail.com>
2018-08-19 21:57:21 +0300

    Removed User Controller stuff

--- bootstrap/router.php
+++ bootstrap/router.php
@@ -7,8 +7,6 @@ $dispatcher = FastRoute\simpleDispatcher(function(RouteCollector $r) {
     $r->addRoute('POST', '/task/save', [new App\Controllers\TaskController, 'save']);
     $r->addRoute('GET', '/task/{id:\d+}', [new App\Controllers\TaskController, 'show']);
     $r->addRoute('GET', '/', [new App\Controllers\TaskController, 'index']);
-
-    $r->addRoute('GET', '/user/create_random', [new App\Controllers\UserController, 'createRandom']);
 });
 
 // Fetch method and URI from somewhere
