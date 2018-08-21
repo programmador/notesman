1aab5f044965dec51c287ca4c6563a023c0da96f
Alexander Konotop <alexander.konotop@gmail.com>
2018-08-21 23:03:15 +0300

    Implemented image resizing

--- app/Controllers/TaskController.php
+++ app/Controllers/TaskController.php
@@ -7,6 +7,9 @@ use Exception;
 class TaskController extends AbstractController
 {
 
+    const MAX_IMG_WIDTH = 320;
+    const MAX_IMG_HEIGHT = 240;
+
     public function index()
     {
         $tasks = Task::with('user')->get();
@@ -52,7 +55,68 @@ class TaskController extends AbstractController
             return null;
         }
 
+        list($width, $height) = $imgInfo;
+        if($width > self::MAX_IMG_WIDTH || $height > self::MAX_IMG_WIDTH) {
+            return $this->getResizedImageContent($filePath, $imgInfo);
+        }
         return file_get_contents($filePath);
+    }
+
+    function getResizedImageContent(string $path, array $imgInfo)
+    {
+        $file = $this->createThumbnail($path, $imgInfo['mime']);
+        if(is_resource($file)) {
+            $content = stream_get_contents($file, -1, 0);
+            fclose($file);
+            return $content;
+        } else {
+            return null;
+        }
+    }
+
+    function createThumbnail(string $path, string $mime)
+    {
+        $png = $mime == 'image/png';
+        $jpg = in_array($mime, ['image/jpg', 'image/jpeg', 'image/pjpeg']);
+
+        if($png) {
+            $src = imagecreatefrompng($path);
+        } elseif ($jpg) {
+            $src = imagecreatefromjpeg($path);
+        } else {
+            throw new Exception("Unsupported image format");
+        }
+
+        $oldWidth = imageSX($src);
+        $oldHeight = imageSY($src);
+
+        $ratio = $oldWidth / $oldHeight;
+        $defaultRatio = self::MAX_IMG_WIDTH / self::MAX_IMG_HEIGHT;
+
+        if($ratio > $defaultRatio) {
+            $newWidth = self::MAX_IMG_WIDTH;
+            $newHeight = (int)($newWidth / $ratio);
+        } else {
+            $newHeight = self::MAX_IMG_HEIGHT;
+            $newWidth = (int)($newHeight * $ratio);
+        }
+
+        $dst = ImageCreateTrueColor($newWidth, $newHeight);
+
+        imagecopyresampled($dst, $src, 0, 0, 0, 0, $newWidth, $newHeight, $oldWidth, $oldHeight);
+
+        $descriptor = tmpfile();
+        //$descriptor = fopen(__DIR__ . '/../../storage/tempimage.' . ($jpg ? 'jpg' : 'png'), 'w+');
+        if($png) {
+            imagepng($dst, $descriptor, 8);
+        } elseif ($jpg) {
+            imagejpeg($dst, $descriptor, 80);
+        }
+
+        imagedestroy($dst);
+        imagedestroy($src);
+
+        return $descriptor;
     }
 
 }
